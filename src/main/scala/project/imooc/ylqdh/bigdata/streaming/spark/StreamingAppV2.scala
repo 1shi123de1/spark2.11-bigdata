fa305a6be54aa53bd2432282d6cae86e4c866a1a
1shi123de1 <ylqdh2014@qq.com>
2019-10-08 11:14:14 +0800

    增加offset管理Kafka数据

--- src/main/scala/project/imooc/ylqdh/bigdata/streaming/spark/StreamingAppV2.scala
+++ src/main/scala/project/imooc/ylqdh/bigdata/streaming/spark/StreamingAppV2.scala
@@ -1,14 +1,14 @@
 package project.imooc.ylqdh.bigdata.streaming.spark
 
 import com.alibaba.fastjson.JSON
-import org.apache.spark.SparkConf
-import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.{SparkConf, TaskContext}
+import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import project.imooc.ylqdh.bigdata.streaming.utils.{ParamsConf, RedisPool}
 
 /*
   统计每小时付费的订单数&订单总额
-  或者每分钟
+  或者每分钟的
  */
 object StreamingAppV2 {
   def main(args: Array[String]): Unit = {
@@ -26,6 +26,7 @@ object StreamingAppV2 {
 
     // 数据拿到之后，就可以进行统计分析了
     stream.foreachRDD(rdd => {
+
       val data = rdd.map(x=>JSON.parseObject(x.value()))
         .map(x => {
           val flag = x.getString("flag")
@@ -38,9 +39,11 @@ object StreamingAppV2 {
 
           val success:(Long,Long) = if (flag == "1") (1,fee) else (0,0)
 
+
           /*
             把上面的数据规整为一个数据结构
             day,hour,minute 分别是时间粒度
+            (天，小时，分钟，[订单数，是否成功，订单金额] )
            */
           (day,hour,minute,List[Long](1,success._1,success._2))
         })
@@ -72,6 +75,20 @@ object StreamingAppV2 {
       })
 
       })
+
+    // offset
+    stream.foreachRDD( rdd => {
+
+      // 获取offset
+      val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      //打印offset
+      rdd.foreachPartition(iter => {
+        val o = offsetRanges(TaskContext.get.partitionId())
+        println(s"${o.topic} ${o.partition} ${o.fromOffset} ${o.untilOffset}")
+      })
+      // 提交offset
+      stream.asInstanceOf[CanCommitOffsets].commitAsync(offsetRanges)
+    })
 
 
     ssc.start()
