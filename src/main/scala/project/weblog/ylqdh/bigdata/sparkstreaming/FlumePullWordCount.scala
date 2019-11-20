5ce3e7ec6e2b6ebd2029bda10a2226a286b52d2f
1shi123de1 <ylqdh2014@qq.com>
2019-11-20 14:54:42 +0800

    spark streaming对接flume的两种模式测试完成

--- src/main/scala/project/weblog/ylqdh/bigdata/sparkstreaming/FlumePullWordCount.scala
+++ src/main/scala/project/weblog/ylqdh/bigdata/sparkstreaming/FlumePullWordCount.scala
@@ -6,24 +6,23 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /*
   Spark Streaming直接采集flume的avro sink过来的数据
-  用的是push模式，所以需要先启动streaming端，再启动flume
+  用的是pull模式，所以需要先启动flume，再启动streaming端
  */
-object FlumePushWordCount {
+object FlumePullWordCount {
   def main(args: Array[String]): Unit = {
 
     if(args.length != 2) {
-      System.err.println("Usage: FlumePushWordCount <hostname> <port>")
+      System.err.println("Usage: FlumePullWordCount <hostname> <port>")
       System.exit(1)
     }
 
     val Array(hostname,port) = args
 
-    val sparkconf = new SparkConf()//.setMaster("local[*]").setAppName("FlumePushWordCount")
+    val sparkconf = new SparkConf().setMaster("local[*]").setAppName("FlumePullWordCount")
     val ssc = new StreamingContext(sparkconf,Seconds(5))
 
     // TODO... 使用SparkStreaming 整合Flume
-    // 换成 createPollingStream就是pull模式的
-    val flumeStream = FlumeUtils.createStream(ssc,hostname,port.toInt)
+    val flumeStream = FlumeUtils.createPollingStream(ssc,hostname,port.toInt)
 
     flumeStream.map(x => new String(x.event.getBody.array()).trim)   // 获取flume上发送过来的信息
       .flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)    // 对接收到的信息进行wordCount
