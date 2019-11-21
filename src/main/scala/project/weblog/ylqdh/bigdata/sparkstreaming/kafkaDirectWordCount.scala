936c885ae0bd8fb559f4c8c80e275653f7006aa0
1shi123de1 <ylqdh2014@qq.com>
2019-11-21 16:23:52 +0800

    spark streaming对接Kafka测试完成

---
+++ src/main/scala/project/weblog/ylqdh/bigdata/sparkstreaming/kafkaDirectWordCount.scala
@@ -0,0 +1,30 @@
+package project.weblog.ylqdh.bigdata.sparkstreaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import project.imooc.ylqdh.bigdata.streaming.utils.ParamsConf
+
+object kafkaDirectWordCount {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("kafkaDirectWordCount").setMaster("local[*]")
+    val ssc = new StreamingContext(sparkConf,Seconds(5))
+
+    // TODO ... 消费数据
+    // messages 是全部的数据
+    val messages = KafkaUtils.createDirectStream(ssc,
+      LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Subscribe[String,String](ParamsConf.topic,ParamsConf.kafkaParams))
+
+    // x.value获得数据里的值
+    messages.map(x => x.value())
+        .flatMap(_.split(" "))
+        .map(x => (x,1))
+        .reduceByKey(_+_)
+        .print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
