6fcf9507613dba0bda6a697b49397b11aa8c009d
1shi123de1 <ylqdh2014@qq.com>
2019-11-25 11:21:03 +0800

    增加日期工具类

---
+++ src/main/scala/project/weblog/ylqdh/bigdata/streaming/DateUtil.scala
@@ -0,0 +1,23 @@
+package project.weblog.ylqdh.bigdata.streaming
+
+import java.util.Date
+
+import org.apache.commons.lang3.time.FastDateFormat
+
+object DateUtil {
+
+  val YYYYMMDDHHMMSS_FORMAT = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss")
+  val TARGET_FORMAT = FastDateFormat.getInstance("yyyyMMddHHmmss")
+
+  def getTime(time:String) = {
+    YYYYMMDDHHMMSS_FORMAT.parse(time).getTime
+  }
+  def parseToMinute(time:String) =  {
+    TARGET_FORMAT.format(new Date(getTime(time)))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(parseToMinute("2019-11-25 11:07:09"))
+  }
+
+}
